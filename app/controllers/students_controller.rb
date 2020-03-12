8d327bf85a2b8c4abc2813939b24e277d303cbd0
CBreakr <github email address>
2020-03-12 18:29:00 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,15 +11,21 @@ class StudentsController < ApplicationController
   def new
     @first_name = ""
     @last_name = ""
-    if params.has_key? ("format") then
-      student = Student.find(params["format"])
+    if session[:updated_student_id] then
+    # if params.has_key? ("format") then
+      # student = Student.find(params["format"])
+      # byebug
+      student = Student.find(session[:updated_student_id])
       @first_name = student.first_name
       @last_name = student.last_name
+      session[:updated_student] = nil
     end
   end
 
   def create
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to new_student_path(@student)
+    session[:updated_student_id] = @student.id
+    # redirect_to new_student_path(@student)
+    redirect_to new_student_path
   end
 end
