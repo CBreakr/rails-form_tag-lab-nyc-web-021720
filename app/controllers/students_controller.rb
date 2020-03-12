11f570daf6016a5c42fbe8ec42a3a2ac7b6ea597
CBreakr <github email address>
2020-03-12 14:11:49 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,18 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @first_name = ""
+    @last_name = ""
+    if params.has_key? ("format") then
+      student = Student.find(params["format"])
+      @first_name = student.first_name
+      @last_name = student.last_name
+    end
+  end
+
+  def create
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to new_student_path(@student)
+  end
 end
